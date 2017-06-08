d39928f21eda01f708d50bd3d659306b8f6ed7e7
Andrew <Andrew@DESKTOP-46C36BV>
2017-06-08 08:05:44 +0400

    Add comments to OrganizingService.

--- src/main/java/com/gmail/razandale/workperiodsorganizer/OrganizingService.java
+++ src/main/java/com/gmail/razandale/workperiodsorganizer/OrganizingService.java
@@ -17,15 +17,34 @@ public class OrganizingService {
     //Here we store different periods organizers.
     private List<Organizer> organizers;
     
+    
+    /**
+     * Main method to get an organized graph of work.
+     * @param workIntervals
+     * @param employeeIntervals
+     * @return a validated graph of work intervals with employees
+     */
     public List<WorkInterval> organize(
-        List<WorkInterval> workPeriods,
-        List<EmployeeInterval> workRequestPeriods){
+        List<WorkInterval> workIntervals,
+        List<EmployeeInterval> employeeIntervals){
         
-        List<EmployeeInterval> result = workRequestPeriods;
+        List<EmployeeInterval> result = employeeIntervals;
         
         for (Organizer organizer : organizers){
-            result = organizer.organize(workPeriods, result);
+            result = organizer.organize(workIntervals, result);
         }
-        return veld(workPeriods,result);
+        return weld(workIntervals,result);
+    }
+    
+    /**
+     * Clears workIntervals and refills it with employeeIntervals.
+     * @param workIntervals
+     * @param employeeIntervals
+     * @return a work graph of work intervals filled with inployees
+     */
+    private List<WorkInterval> weld(
+        List<WorkInterval> workIntervals,
+        List<EmployeeInterval> employeeIntervals){
+        throw new RuntimeException("Method veld is not implemented yet.");
     }
 }
